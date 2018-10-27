f1a30b1b854da76c7785de0b5c47fc32738e4719
Ruben <rbndeheer@gmail.com>
2018-10-27 23:06:44 +0200

    generatekar timer

--- goudkoorts/Countdown.cs
+++ goudkoorts/Countdown.cs
@@ -9,6 +9,7 @@ namespace goudkoorts
     public class Countdown
     {
         Timer MoveTimer;
+        Timer GenerateTimer;
         private GameController gc;
 
         public Countdown(GameController gc)
@@ -17,11 +18,21 @@ namespace goudkoorts
             MoveTimer = new Timer(2000);
             MoveTimer.Elapsed += MoveTimer_elapsed;
             MoveTimer.Start();
+
+            GenerateTimer = new Timer(10000);
+            GenerateTimer.Elapsed += GenerateTimer_elapsed;
+            GenerateTimer.Start();
         }
 
         private void MoveTimer_elapsed(object sender, ElapsedEventArgs e)
         {
             gc.MoveAnimatie();
+        }
+
+        private void GenerateTimer_elapsed(object sender, ElapsedEventArgs e)
+        {
+            gc.game.GenereerKar();
+   
         }
     }
 }
